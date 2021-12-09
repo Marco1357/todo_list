03affe14c1b030f1a522b87715f58f6a186454ea
Marco <miranda_garcya@hotmail.com>
2021-12-09 14:03:12 -0300

    Criação do setp inicial do projeto todo_list

---
+++ lista/core/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Pagina(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True)
